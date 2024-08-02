31694fa1275e33ae6ae821e5fc302593326b49ad
FilipeDiass <diasjoaum@gmail.com>
2024-08-02 12:32:43 -0300

    zod config variables envroiments

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,12 @@
 import { z } from 'zod'
+
+const nonEmptyString = z
+  .string()
+  .min(1, { message: 'Set the environment variable' })
+
+const envSchema = z.object({
+  PORT: nonEmptyString.transform((val) => Number(val)),
+  NODE_ENV: nonEmptyString,
+})
+
+export const env = envSchema.parse(process.env)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import server from './server/server'
+import { env } from './env'
 
-// port: process.env.PORT ?? 3333
-server.listen({ port: 3333 }, (err, address) => {
+server.listen({ port: env.PORT || 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
